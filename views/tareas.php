832a4d8595da64b874adfa3272e59661528bddac
Gera <go47455@gmail.com>
2025-07-16 23:27:38 -0600

    tareas css

--- views/tareas.php
+++ views/tareas.php
@@ -34,6 +34,7 @@ $tareas = $result->fetch_all(MYSQLI_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tareas</title>
+    <link rel="stylesheet" href="../styles/tareas.css">
 </head>
 <body>
     <h2>Tus Tareas</h2>
