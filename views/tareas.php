834fc62934cea8952bfc635f782d634e96c4ea5e
Gera <go47455@gmail.com>
2025-07-24 16:44:17 -0600

    detalle toggle

--- views/tareas.php
+++ views/tareas.php
@@ -34,7 +34,7 @@ $tareas = $result->fetch_all(MYSQLI_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tareas</title>
-    <link rel="stylesheet" href="../styles/tareas.css?v=1.3">
+    <link rel="stylesheet" href="../styles/tareas.css?v=1.4">
 </head>
 <body>
     <header class="app-header">
